6855f48dcbfdd81d8a073a947558456715a0199d
joejones76 <josephjones7@gmail.com>
2020-04-06 12:08:16 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+class Author 
+    attr_accessor :name 
+
+    def intitalize (name) 
+        @name = name 
+    end 
+
+
+end 
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+    attr_accessor :title, :artist 
+
+    def intitalize (title, artist) 
+    @title = title 
+    @artist = artist
+    end 
+
+end
